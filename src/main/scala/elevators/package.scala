0ce006bb9f28e18ebb30104a45660ec4e4345df1
Petter Arvidsson <petter.arvidsson@gmail.com>
2016-11-10 17:05:44 +0100

    Add documentation for package functions

--- src/main/scala/elevators/package.scala
+++ src/main/scala/elevators/package.scala
@@ -1,6 +1,13 @@
 package elevators
 
 object `package` {
+
+  /** Returns an updated ElevatorGroup with an elevator selected to handle the request.
+    *
+    * @param floor the requested floor to be serviced.
+    * @param direction the direction of travel after entering the elevator.
+    * @param group the elevator group to be updated.
+    */
   def floorRequest(floor: Floor, direction: Direction, group: ElevatorGroup)(
       implicit config: ElevatorConfig): ElevatorGroup = {
     val bestIndex = group.elevators
@@ -15,11 +22,21 @@ object `package` {
     group.copy(elevators = group.elevators.updated(bestIndex, updatedElevator))
   }
 
+  /** Returns an update ElevatorGroup which has been increment a given
+    * number of steps.
+    *
+    * @param group the elevator group to be updated.
+    * @param steps the number of steps to iterate the simulation.
+    */
   def step(group: ElevatorGroup, steps: Int): ElevatorGroup = {
     val updatedElevators = group.elevators.map(_.step(steps))
     group.copy(elevators = updatedElevators)
   }
 
+  /** Returns an update ElevatorGroup which has been incremented one step.
+    *
+    * @param group the elevator group to be updated.
+    */
   def step(group: ElevatorGroup): ElevatorGroup =
     step(group, 1)
 
